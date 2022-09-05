52ad389948ace38acb371cb6094399817fb0e33e
Kavandeep <kavan1@live.co.uk>
2022-09-05 19:04:33 +0100

    added methods to ToDoService

--- src/main/java/com/example/todo/ToDoRepository.java
+++ src/main/java/com/example/todo/ToDoRepository.java
@@ -1,11 +1,19 @@
 package com.example.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ToDoRepository extends JpaRepository<ToDo, Integer> {
 
+    List<ToDo> findToDoByCreatedBy(String creatingBy);
 
+    @Modifying
+    @Query("delete ToDo t where t.createdBy = :createdBy")
+    int deleteToDoByCreatedBy(String createdBy);
 
 }
--- src/main/java/com/example/todo/ToDoService.java
+++ src/main/java/com/example/todo/ToDoService.java
@@ -1,10 +1,62 @@
 package com.example.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ToDoService {
 
+    @Autowired
+    ToDoRepository toDoRepository;
 
+    public List<Integer> extractIds () {
+        List<ToDo> toDos = toDoRepository.findAll();
+        return toDos.stream().map(toDo -> toDo.getId()).collect(Collectors.toList());
+    }
 
+    public ToDo getById(int id) {
+        ToDo toDo = toDoRepository.findById(id).orElse(null);
+        return toDo;
+    }
+
+    public List<ToDo> getAll() {
+        List<ToDo> toDos = toDoRepository.findAll();
+        return toDos;
+    }
+
+    public List<ToDo> getToDosCreatedBy(String createdBy){
+        List<ToDo> toDos = toDoRepository.findToDoByCreatedBy(createdBy);
+        return toDos;
+    }
+
+    public ToDo createToDo(ToDo toDo) {
+        if (toDo.getText() == null || toDo.getText().length() < 1) {
+            throw new RuntimeException("ToDo must have text");
+        }
+
+        if (toDo.getCreatedBy() == null || toDo.getCreatedBy().length() < 1) {
+            throw new RuntimeException("ToDo must have created by");
+        }
+
+        return toDoRepository.save(toDo);
+    }
+
+    public boolean deleteById(int id){
+        ToDo toDoToDelete = toDoRepository.findById(id).orElse(null);
+
+        if(toDoToDelete == null){
+            return false;
+        }
+
+        toDoRepository.delete(toDoToDelete);
+        return true;
+    }
+
+    public int deleteToDosByCreatedBy(String createdBy) {
+        int deletedCount = toDoRepository.deleteToDoByCreatedBy(createdBy);
+        return deletedCount;
+    }
 }
